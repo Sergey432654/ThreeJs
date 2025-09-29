678b3ef1b93468c18fb14b7fb7dc6bbd00f08a83
Serhii Yemets <115184910+Sergey432654@users.noreply.github.com>
2025-09-29 20:56:34 +0200

    cube

---
+++ index.js
@@ -0,0 +1,26 @@
+import * as THREE from 'three'
+
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75, 
+    window.innerWidth / window.innerHeight,
+    0.1,
+    100,
+)
+camera.position.z = 5 ;
+const renderer  = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth,window.innerHeight);
+document.body.appendChild(renderer.domElement);
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({color: 'red' });
+const cube = new THREE.Mesh(geometry,material);
+scene.add(cube);
+
+
+function animate(){
+    requestAnimationFrame(animate);
+    renderer.render(scene,camera);
+}
+animate();
